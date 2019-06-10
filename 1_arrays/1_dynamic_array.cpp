c0d480885232f069edd7e7d50a72b11c0f99fd2c
JiwoonKim <diana0501@naver.com>
2019-06-10 21:20:18 +0900

    1-1. dynamic array 코드 완료

--- 1_arrays/1_dynamic_array.cpp
+++ 1_arrays/1_dynamic_array.cpp
@@ -15,6 +15,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ *  define class for dynamic array
+ */
 class DynamicArr {
 private:
     int *arr;
@@ -22,33 +25,47 @@ private:
     size_t capacity;
 
 public:
+
+    // constructor function
     DynamicArr(size_t capacity) {
+        arr = new int[capacity];
         this->size = 0;
         this->capacity = capacity;
     }
+
+    // return value at index
     int getValueAtIndex(size_t index) {
         return arr[index];
     }
+
+    // set value at index
     void setValueAtIndex(size_t index, int value) {
         if (index >= capacity) {
             throw invalid_argument("index out of boundary");
         }
         arr[index] = value;
     }
+
+    // push value to end of array
     void pushBack(int value) {
         if (size == capacity) {
             resize();
         }
         arr[size++] = value;
     }
-    void remove(size_t index) {
+
+    // remove value at index (shift items to fill blank space)
+    void removeValueAtIndex(size_t index) {
         if (index >= capacity || index < 0) {
             throw invalid_argument("index out of boundary");
         }
         for (int i = index + 1; i < size; i++) {
             arr[i - 1] = arr[i];
         }
+        size--;
     }
+
+    // resize array to twice the capacity
     void resize() {
         this->capacity *= 2;
         int *tempArr = new int[capacity];
@@ -56,6 +73,8 @@ public:
             tempArr[i] = arr[i];
         }
     }
+
+    // return the number of items in the array
     size_t getSize() {
         return this->size;
     }
