9feb943ee72aa17c732405117875ceeb4daba54b
JiwoonKim <diana0501@naver.com>
2019-06-11 16:10:36 +0900

    1-1. dynamic array 코드 및 필기 완료

--- 1_arrays/1_dynamic_array.cpp
+++ 1_arrays/1_dynamic_array.cpp
@@ -1,14 +1,32 @@
 /**
  *  Implement a dynamic array
  * 
- *  Time Complexity: O(
- *  Space Complexity: O(
+ *  * Dynamic Array
+ *    : flexible array (resizable/mutable)
+ *      -> based on dynamically-allocated arrays
+ *      => a.k.a VECTOR in C++ STL
  * 
- *  * What I learned:
+ *    - append: amortized O(1) 
+ *              -> worst-case O(n) bcuz of copying elements
+ *    - lookup: O(1) 
+ *              -> random access (by index)
+ *    - insert: O(n)
+ *              -> need to scoot elements
+ *    - delete: O(n)
+ *              -> need to scoot elements
  * 
- *    ** define class of dynamic array
- *       - take a good look at how a graph class is structured!
- *       - use it as reference /example for future
+ *  ** What I learned
+ * 
+ *     * how to define dynamic array (using 'new')
+ * 
+ *     * how to SCOOT ELEMENTS
+ *       - when scooting elements to the RIGHT (for insertion)
+ *         -> start from end of array to position
+ *         -> use arr[i] = arr[i - 1] or arr[i + 1] = arr[i]
+ * 
+ *       - when scooting elements to the LEFT (for deletion)
+ *         -> start from position to end of array
+ *         -> use arr[i] = arr[i + 1] or arr[i - 1] = arr[i]
  * 
  */
 
@@ -34,19 +52,14 @@ public:
     }
 
     // return value at index
-    int getValueAtIndex(size_t index) {
-        return arr[index];
-    }
-
-    // set value at index
-    void setValueAtIndex(size_t index, int value) {
+    int valueAt(size_t index) {
         if (index >= capacity) {
             throw invalid_argument("index out of boundary");
         }
-        arr[index] = value;
+        return arr[index];
     }
 
-    // push value to end of array
+    // push value to end of array (append)
     void pushBack(int value) {
         if (size == capacity) {
             resize();
@@ -54,8 +67,23 @@ public:
         arr[size++] = value;
     }
 
+    // insert value at index
+    void insertAt(size_t index, int value) {
+        if (index >= capacity || index < 0) {
+            throw invalid_argument("index out of boundary");
+        }
+        if (size == capacity) {
+            resize();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[index] = value;
+        size++;
+    }
+
     // remove value at index (shift items to fill blank space)
-    void removeValueAtIndex(size_t index) {
+    void removeAt(size_t index) {
         if (index >= capacity || index < 0) {
             throw invalid_argument("index out of boundary");
         }
@@ -75,7 +103,7 @@ public:
     }
 
     // return the number of items in the array
-    size_t getSize() {
+    size_t arraySize() {
         return this->size;
     }
 };
