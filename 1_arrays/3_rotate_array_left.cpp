7942e486072ba5c5aac3b1f032d1f61e04952acd
JiwoonKim <diana0501@naver.com>
2019-06-12 15:07:35 +0900

    1-3. rotate array left 시작

---
+++ 1_arrays/3_rotate_array_left.cpp
@@ -0,0 +1,24 @@
+/**
+ *  Given an array, rotate the array to the right by k steps.
+ *  (k is non-negative)
+ * 
+ *  * Solutions
+ *    
+ *    (1) brute-force method: O(n * k) time, O(1) space
+ *        - rotate one-by-one for k times
+ * 
+ *    (2) 
+ * 
+ *  ** What I learned
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ *  
+ */
+void rotate(vector<int>& nums, int k) { 
+
+}
