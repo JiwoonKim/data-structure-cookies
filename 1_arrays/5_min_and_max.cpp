aa0efc458650c06dc976ab1a02db08f222d79fb1
JiwoonKim <diana0501@naver.com>
2019-08-10 00:39:32 +0900

    1-5. min and max 코드 및 필기 추가

--- 1_arrays/5_min_and_max.cpp
+++ 1_arrays/5_min_and_max.cpp
@@ -1,7 +1,13 @@
 /**
- *  Given 
+ *  Given an array of integers, find the min and max value respectively.
  * 
  *  * Solutions
+ * 
+ *    (1) greedy algorithm: O(n) time, O(1) space
+ *        -> iterate over each item while keeping track of min and max value so far
+ * 
+ *    (2) sort: O(n log n) time, O(n) or O(1) time
+ *        -> 
  * 
  * 
  *  ** What I learned
@@ -13,6 +19,24 @@
 using namespace std;
 
 /**
- *  (1) 
+ *  (1) greedy algorithm
  */
-int findLeftShift(vector<int> v) {
+pair<int, int> findMinMax(vector<int> v) {
+
+    // keep track of min and max value so far
+    int min_val = v[0];
+    int max_val = v[0];
+
+    // iterate over items and update min and max
+    for (auto x : v) {
+        min_val = min(min_val, x);
+        max_val = max(max_val, x);
+    }
+
+    return make_pair(min_val, max_val);
+}
+
+/**
+ *  (2) sort
+ */
+pair<int, int> findMinMax(vector<int> v) {
