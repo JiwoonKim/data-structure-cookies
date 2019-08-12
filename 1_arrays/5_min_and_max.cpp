abc1af8607598c694a9eb84d0479e50323a5b021
JiwoonKim <diana0501@naver.com>
2019-08-12 18:37:53 +0900

    1-4. min and max 코드 및 필기 완료

--- 1_arrays/5_min_and_max.cpp
+++ 1_arrays/5_min_and_max.cpp
@@ -7,12 +7,21 @@
  *        -> iterate over each item while keeping track of min and max value so far
  * 
  *    (2) sort: O(n log n) time, O(n) or O(1) time
- *        -> 
+ *        -> sort the entire array and return the first and last value
+ * 
+ *    (3) STL min_element & max_element: O(n) time, O(1) space
+ *        -> use STL functions to find ITERATOR of each
  * 
  * 
- *  ** What I learned
+ *  ** What I learned: 
  * 
- *  
+ *     ** bonus: kth min & max
+ *        (1) greedy algorithm: O(n) time, O(k) space
+ *            - iterate over each item and keep track of k min and k max values so far
+ * 
+ *        (2) sort: O(n log n) time, O(n) or O(1) space
+ *            - sort all of the items and return the v[k - 1] and v[n - k - 1]
+ *   
  */
 
 #include <bits/stdc++.h>
@@ -40,3 +49,19 @@ pair<int, int> findMinMax(vector<int> v) {
  *  (2) sort
  */
 pair<int, int> findMinMax(vector<int> v) {
+
+    sort(v.begin(), v.end());
+    return make_pair(v[0], v[v.size() - 1]);
+}
+
+/**
+ *  (3) STL min_element & max_element
+ */
+pair<int, int> findMinMax(vector<int> v) {
+
+    // find the iterators for min and max values
+    auto minIt = min_element(v.begin(), v.end());
+    auto maxIt = max_element(v.begin(), v.end());
+
+    return make_pair(*minIt, *maxIt);
+}
