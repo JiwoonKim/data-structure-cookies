f2694b6d458c003c0c3860e551c15d75b217f09f
JiwoonKim <diana0501@naver.com>
2019-08-17 20:15:54 +0900

    3-1. stack 코드 완료

--- 3_stacks/1_stack.cpp
+++ 3_stacks/1_stack.cpp
@@ -1,5 +1,5 @@
 /**
- *  Implement a stack using a dynamic array
+ *  Implement a stack using a dynamic allocated array
  * 
  *  * 
  * 
@@ -13,40 +13,67 @@ using namespace std;
  *  define class for stack
  */
 class Stack {
+    private:
+        int* stackArr;
+        size_t size;
+        size_t capacity;
 
-    vector<int> stack_;
-    size_t size_ = 0;
-
-    // push value into stack
-    void push(int value) {
-        stack_[size_] = value;
-        size_++;
-    }
-
-    // pop value from stack
-    void pop() {
-        if (size_ == 0) {
-            throw invalid_argument("cannot pop from empty stack!");
+    public:
+        // constructor method
+        Stack(size_t capacity) {
+            this->stackArr = new int[capacity];
+            this->size = 0;
+            this->capacity = capacity;
         }
-        size_--;
-    }
 
-    // return top value
-    int top() {
-        if (size_ == 0) {
-            throw invalid_argument("stack is empty!");
+        // resize method
+        int* resize() {
+            /** create space for twice the capacity of original
+             *  and move all elements to new space
+             */
+            int* temp = new int[this->capacity * 2];
+            for (int i = 0; i < this->size; i++) {
+                temp[i] = this->stackArr[i];
+            }
+
+            // delete original and set pointer to new space
+            delete stackArr;
+            this->stackArr = temp;
         }
-        return stack_[size_ - 1];
-    }
 
-    // return number of elements in stack
-    size_t size() {
-        return size_;
-    }
+        // push value into stack
+        void push(int value) {
+            if (this->size == this->capacity) {
+                this->stackArr = resize();
+            }
+            this->stackArr[this->size] = value;
+            this->size++;
+        }
 
-    // check if stack is empty or not
-    bool isEmpty() {
-        if (size_ == 0) return true;
-        return false;
-    }
+        // pop value from stack
+        void pop() {
+            if (this->size == 0) {
+                throw invalid_argument("cannot pop from empty stack!");
+            }
+            this->size--;
+        }
+
+        // return top value
+        int top() {
+            if (this->size == 0) {
+                throw invalid_argument("stack is empty!");
+            }
+            return this->stackArr[this->size - 1];
+        }
+
+        // return number of elements in stack
+        size_t size() {
+            return this->size;
+        }
+
+        // check if stack is empty or not
+        bool isEmpty() {
+            if (this->size == 0) return true;
+            return false;
+        }
 };
