92e5d5802ce74e4b3db427e3342a08221616ad6f
JiwoonKim <diana0501@naver.com>
2019-10-01 17:35:22 +0900

    3-5. simple text editor 필기 추가

--- 3_stacks/5_simple_text_editor.cpp
+++ 3_stacks/5_simple_text_editor.cpp
@@ -15,9 +15,13 @@
  * 
  *    (2) use two stacks: O(m) time, O(n + m) extra space
  *        -> use two stacks to represent total string
- *           -> cursor is tracked by where the two stacks diverge
- *              ex. stack 1 = "ab", stack2 = "cd" -> then, cursor at "ab | cd"
+ *        -> cursor is tracked by where the two stacks diverge
+ *           ex. stack 1 = "ab", stack2 = "cd" -> then, cursor at "ab | cd"
  *        -> insert and delete at stack 1's top
+ * 
+ *    (3) use list: O(m) time, O(n + m) extra space
+ *        -> use linked list to represent total string
+ *        -> cursor is tracked by iterator pointing to a node in the list
  * 
  * 
  *  ** What I learned
@@ -76,6 +80,45 @@ string editText(string s, vector<string> commands) {
     while (!right.empty()) {
         editted += right.top();
         right.pop();
+    }
+    return editted;
+}
+
+/**
+ *  (3) use list
+ */
+string editText(string s, vector<string> commands) {
+    // construct string list and cursor
+    list<char> ll(s.begin(), s.end());
+    auto cursor = ll.end();
+    
+    // execute given commands
+    for (string command : commands) {
+        /** move cursor left or right
+         *  by moving left's top to right's top
+         */ 
+        if (command[0] == 'L' && cursor != ll.begin()) {
+            cursor--;
+        }
+        if (command[0] == 'R' && cursor != ll.end()) {
+            cursor++;
+        }
+        // insert new char at cursor
+        if (command[0] == 'I') {
+            ll.insert(cursor, command[2]);
+            cursor++;
+        }
+        // delete char at cursor
+        if (command[0] == 'D' && !ll.empty()) {
+            auto temp = cursor;
+            temp--;
+            ll.erase(temp);
+        }
+    }
+
+    string editted = "";
+    for (char c : ll) {
+        editted += c;
     }
     return editted;
 }
