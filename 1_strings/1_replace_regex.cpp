b386e59aad30ce21480d0da8a9a76db35a12b3f2
JiwoonKim <diana0501@naver.com>
2020-01-12 17:12:51 +0900

    3-6. queue with two stacks 코드 추가

---
+++ 1_strings/1_replace_regex.cpp
@@ -0,0 +1,20 @@
+/**
+ *  Use 
+ * 
+ *  * Solution
+ * 
+ *    - use stack to reverse orders of given data structures
+ * 
+ * 
+ *  ** What I learned
+ * 
+ *     ** stack is useful for REVERSING ORDERS
+ * 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ *  r
+ */
--- 3_stacks/6_queue_with_two_stacks.cpp
+++ 3_stacks/6_queue_with_two_stacks.cpp
@@ -1,32 +1,80 @@
 /**
- *  Given a certain prefix/infix/postfix expression,
- *  convert it to either a prefix/infix/postfix expression.
+ *  Implement a queue data structure using two stacks.
  * 
  *  * Solution
  * 
- *    (1) use stack: O
+ *    (1) in-stack & out-stack: O(1) push & pop
+ *        - use each stack for different purposes
+ *          - push: push new element into the in-stack
+ *          - pop: pop new element from out-stack
+ *                 if out-stack is empty, 
  * 
  * 
  *  ** What I learned
  * 
  * 
 */
-
 #include <bits/stdc++.h>
 using namespace std;
 
 /**
  *  
  */
-string infixToPostfix(string s) {
+class Queue {
+    private: 
+        stack<int> inStack;
+        stack<int> outStack;
 
-    string result;
+    public:
+        // constructor method
+        Queue() {
 
-    stack<char> operands;
-    stack<char> operators;
+        }
 
-    unordered_set<char> operators = { '+', }
-    for (char c : s) {
+        // push value into queue
+        void enqueue(int value) {
+            this->inStack.push(value);
+        }
 
-    }
-}
+        // pop value from stack
+        void pop() {
+            moveInFromOut();
+            if (this->outStack.empty()) {
+                throw invalid_argument("cannot dequeue from empty queue!");
+            }
+            this->outStack.pop();
+        }
+
+        // check the front value in the queue
+        int front() {
+            moveInFromOut();
+            if (this->outStack.empty()) {
+                throw invalid_argument("queue is empty!");
+            }
+            return this->outStack.top();
+        }
+
+        /** 
+         *  helper function: enable access to front of queue
+         */ 
+        void moveInFromOut() {
+            /** if out-stack is empty, move in-stack elements to out-stack
+             *  (this reverses the order of the elements to FIFO order)
+             */ 
+            if (!this->outStack.empty()) {
+                while(!this->inStack.empty()) {
+                    int topElement = this->inStack.top();
+                    this->inStack.pop();
+                    this->outStack.push(topElement);
+                }
+            }
+        }
+
+        // check if stack is empty or not
+        bool isEmpty() {
+            if (this->outStack.empty() && this->inStack.empty()) {
+                return true;
+            }
+            return false;
+        }
+};
